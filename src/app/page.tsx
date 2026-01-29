dc0936b6bef6617018b6297fb7f8963cf851b75f
Johny Vilaña <johnyv1305@gmail.com>
2026-01-28 23:41:34 -0500

    Fix: Remove extra code from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,18 +23,3 @@ export default function Home() {
     </>
   );
 }
-
-                background: "#146ef5",
-                color: "#ffffff",
-                boxShadow:
-                  "0px 0.5px 1px rgba(0, 0, 0, 0.25), inset 0px 29px 23px -16px rgba(255, 255, 255, 0.04), inset 0px 0.5px 0.5px rgba(255, 255, 255, 0.2)",
-              }}
-            >
-              Get Started
-            </Link>
-          </div>
-        </Block>
-      </Block>
-    </Section>
-  );
-}
